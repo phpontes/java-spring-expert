2c523caa3256d0a25a4d4652a8f43dfc13f12671
phpontes <ppontes95@hotmail.com>
2024-06-20 08:06:44 -0300

    Product entity, SQL script

---
+++ backend/bin/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_category(name, created_At) VALUES ('Livraiada', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Eletronicaiada', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Computadorzaiada', NOW());
