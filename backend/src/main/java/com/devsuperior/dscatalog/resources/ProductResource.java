18d79591cdb535ce5495d97a477043d5f060f3cf
phpontes <ppontes95@hotmail.com>
2024-09-20 10:16:39 -0300

    Consulta final de produtos e categorias com JOIN FETCH

--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
-import com.devsuperior.dscatalog.projections.ProductProjection;
 import com.devsuperior.dscatalog.services.ProductService;
 
 import jakarta.validation.Valid;
@@ -32,11 +31,11 @@ public class ProductResource {
 	private ProductService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<ProductProjection>> findAll(
+	public ResponseEntity<Page<ProductDTO>> findAll(
 			@RequestParam(value = "name", defaultValue = "") String name,
 			@RequestParam(value = "categoryId", defaultValue = "0") String categoryId,
 			Pageable pageable) {
-		Page<ProductProjection> list = service.findAllPaged(name, categoryId, pageable);
+		Page<ProductDTO> list = service.findAllPaged(name, categoryId, pageable);
 		
 		return ResponseEntity.ok().body(list);
 	}
--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -96,14 +97,21 @@ public class ProductService {
 	}
 	
 	@Transactional(readOnly = true)
-	public Page<ProductProjection> findAllPaged(String name, String categoryId, Pageable pageable) {
+	public Page<ProductDTO> findAllPaged(String name, String categoryId, Pageable pageable) {
 		
 		List<Long> categoryIds = Arrays.asList();
 		if (!"0".equals(categoryId)) {
 			categoryIds = Arrays.asList(categoryId.split(",")).stream().map(Long::parseLong).toList();
 		}
 		
-		return repository.searchProducts(categoryIds, name, pageable);
+		Page<ProductProjection> page = repository.searchProducts(categoryIds, name, pageable);
+		List<Long> productIds = page.map(x -> x.getId()).toList();
+		
+		List<Product> entities = repository.searchProductsWithCategories(productIds);
+		List<ProductDTO> dtos = entities.stream().map(p -> new ProductDTO(p, p.getCategories())).toList();
+		
+		Page<ProductDTO> pageDto = new PageImpl<>(dtos, page.getPageable(), page.getTotalElements());
+		return pageDto;
 	}
 
 }
